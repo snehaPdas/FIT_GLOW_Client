9526f779d0d406c5dfb327d72f99712a1940f527
sneha p <snehapdas777@gmail.com>
2025-02-22 12:28:18 +0530

    google auth corrected

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,13 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { PersistGate } from "redux-persist/integration/react";
 import { SocketContextProvider } from './context/socket';
 import { NotificationProvider } from './context/NotificationContext';
+const clientId = import.meta.env.VITE_CLIENTID; 
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 
   <Provider store={store} >
-      <GoogleOAuthProvider clientId="416524776308-vklojou22b7r4r5cuimv0p9ms3hif5np.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId={clientId}>
       <NotificationProvider>
       <PersistGate loading={null} persistor={persistor}>
       <SocketContextProvider>
